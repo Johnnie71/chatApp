9da45920340f5a2a40fc9cee2c6e68074dc8daf1
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-16 17:31:53 -0400

    cleaned up code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,12 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('Count has been updated!', count)
-})
+// socket.on('countUpdated', (count) => {
+//     console.log('Count has been updated!', count)
+// })
 
-const button = document.querySelector('#increment');
+// const button = document.querySelector('#increment');
 
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    socket.emit('increment')
-})
+// button.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     socket.emit('increment')
+// })
--- src/index.js
+++ src/index.js
@@ -13,17 +13,15 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
     console.log('New web socket connection')
 
-    socket.emit('countUpdated', count);
+    // socket.emit('countUpdated', count);
 
-    socket.on('increment', () => {
-        count++;
-        io.emit('countUpdated', count)
-    })
+    // socket.on('increment', () => {
+    //     count++;
+    //     io.emit('countUpdated', count)
+    // })
 })
 
 server.listen(port, () => {
