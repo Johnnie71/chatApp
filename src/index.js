ce47b47493fb7d80ee386cb54ae7e60627b20d08
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-16 16:31:51 -0400

    requiring http then creating the server using the app const and http

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app)
 
 const port = process.env.PORT || 3000;
 
