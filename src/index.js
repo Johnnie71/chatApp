5907ae986d56c0f53d4058769873e6dc7acc9bba
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-17 15:39:44 -0400

    letting users know another has left the chat

--- src/index.js
+++ src/index.js
@@ -16,13 +16,18 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     // send message to the current user 
     socket.emit('message', 'Welcome!');
-    
+
     // sends message to every user but the current user
     socket.broadcast.emit('message', 'A new user has joined!')
 
     //server listens for client side message and sends a universal message to all
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
+    })
+
+    //server listens to see if someone leaves the chat and lets the other users know!
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left!')
     })
 
 })
