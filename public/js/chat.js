d3bd7beb9f397a55eb16c49f5786b150fa358d7a
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-22 17:23:57 -0400

    formatting the time to be sent

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,7 @@ socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
         message: message.text,
-        createdAt: message.createdAt
+        createdAt: moment(message.createdAt).format('h:mm a')
     });
 
     //inserting messages before the end of the template and inserting the HTML
