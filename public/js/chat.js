c62e8741709068ceb9f6b3647d083e4c3519345e
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-24 16:39:04 -0400

    adjustments

--- public/js/chat.js
+++ public/js/chat.js
@@ -35,7 +35,7 @@ const autoscroll = () => {
     const scrollOffset = $messages.scrollTop + visibleHeight
 
     if(containerHeight - newMessageHeight <= scrollOffset){
-        
+        $messages.scrollTop = $messages.scrollHeight;
     }
 }
 
