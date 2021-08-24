d0abb4248a6123fa695ab094ea6169d9147c4255
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-24 14:58:49 -0400

    adding  for system messages

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
-        socket.emit('message', generateMessage('Welcome!'));
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
+        socket.emit('message', generateMessage('Admin', 'Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`));
         
         callback();
     })
