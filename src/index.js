6dc43033073413f4110b9e37104f330092c984b2
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-24 15:22:47 -0400

    getting the room data of all users in that room when disconnecting from room

--- src/index.js
+++ src/index.js
@@ -52,6 +52,10 @@ io.on('connection', (socket) => {
 
        if(user){
         io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
        }
 
         
